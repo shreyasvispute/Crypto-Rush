03e7814810fb2cb61211018b2cd699a931c21d39
shreyasvispute <vispute.shreyas1@gmail.com>
2022-05-06 16:56:19 -0400

    modified auth js

--- cryptorush/src/firebase/auth.js
+++ cryptorush/src/firebase/auth.js
@@ -1,7 +1,7 @@
 import React, { useState, createContext, useEffect, useContext } from "react";
 import axios from "axios";
 
-import { auth } from "../firebase/FirebaseConfig";
+import { auth } from "./FirebaseConfig";
 import {
   signInWithEmailAndPassword,
   signOut,
