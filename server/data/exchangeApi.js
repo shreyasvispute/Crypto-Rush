b871c068f4e32ab740c9e9d87519109a01b3ec75
alokit25 <alokitgupta98@gmail.com>
2022-05-12 23:03:29 -0400

    Entire exchanges backend and exchange list page frontend code

--- server/data/exchangeApi.js
+++ server/data/exchangeApi.js
@@ -1,6 +1,5 @@
 const axios = require("axios");
 const validations = require("./validations");
-require("dotenv").config();
 
 // Import coingecko-api
 const CoinGecko = require('coingecko-api');
@@ -10,12 +9,15 @@ const CoinGeckoClient = new CoinGecko();
 
 async function getExchange() {  
   // Make calls
-  let pData = await CoinGeckoClient.ping();
-  //console.log(pData)
-  let eData = await CoinGeckoClient.exchanges.all();
-  //console.log(eData)
-  if(eData.code===200){
-    return eData;
+  let baseUrl1 = 'https://api.coingecko.com/api/v3/exchanges?per_page=250&page=1'
+  const  data1  = await axios.get(baseUrl1)
+  let baseUrl2 = 'https://api.coingecko.com/api/v3/exchanges?per_page=250&page=2'
+  const  data2  = await axios.get(baseUrl2)
+  if(data1.status===200 && data2.status===200){
+    let temp = [...data1.data, ...data2.data]
+    //console.log(temp.length)
+    return temp;
+    
   }else{
     throw {
       response: { status: 404, statusText: `No data found.` },
@@ -23,6 +25,115 @@ async function getExchange() {
   }
 };
 
+// async function getSearchData(searchTerm){
+//   let baseUrl = `https://api.coingecko.com/api/v3/search?query=${searchTerm}`
+//   const data = await axios.get(baseUrl)
+//   if(data.data.exchanges.length > 0){
+//     return data.data.exchanges
+//   }else{
+//     throw {
+//       response: { status: 404, statusText: `No data found.` },
+//     }
+//   }
+
+// }
+
+async function searchExchange(searchTerm) {
+  validations.validateString(searchTerm, "Search Term");
+  let ids = [];
+  let result = [];
+
+  let mappingData = await getExchange();
+
+  for (let i = 0; i < mappingData.length; i++) {
+    if (
+      mappingData[i].id.toUpperCase() == searchTerm.toUpperCase() ||
+      mappingData[i].name.toLowerCase() == searchTerm.toLowerCase()
+    ) {
+      result.push(mappingData[i]);
+    }
+  }
+
+  // for (let i = 0; i < ids.length; i++) {
+  //   let id = ids[i];
+  //   let quotesURL = CMC_QUOTES_API_URL.replace("ID", id);
+  //   const { data } = await axios.get(quotesURL, {
+  //     headers: {
+  //       "X-CMC_PRO_API_KEY": CMC_API_KEY,
+  //     },
+  //   });
+  //   if (data.status.error_code === 0) {
+  //     let cryptoData = data.data;
+  //     let metadata = await getCryptoMetadataById(id);
+  //     result.push(Object.assign(cryptoData[id], metadata[id]));
+  //   } else {
+  //     throw {
+  //       response: { status: 404, statusText: `No data found.` },
+  //     };
+  //   }
+  // }
+
+  if (result.length > 0) {
+    return result;
+  } else {
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    };
+  }
+}
+
+
+async function getExchangeList(){
+  // Make calls
+  let pData = await CoinGeckoClient.ping();
+  //console.log(pData)
+  let lData = await CoinGeckoClient.exchanges.list();
+  //console.log(lData)
+  if(lData.code===200){
+    return lData;
+  }else{
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    }
+  }
+}
+
+async function getExchangeById(id){
+  validations.validateString(id, "id");
+  // Make calls
+  let pData = await CoinGeckoClient.ping();
+  //console.log(pData)
+  let idData = await CoinGeckoClient.exchanges.fetch(id);
+  //console.log(idData)
+  if(idData.code===200){
+    return idData;
+  }else{
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    }
+  }
+}
+
+async function getTickers(id){
+  validations.validateString(id, "id");
+  // Make calls
+  let pData = await CoinGeckoClient.ping();
+  //console.log(pData)
+  let tickerData = await CoinGeckoClient.exchanges.fetchTickers(id);
+  //console.log(idData)
+  if(tickerData.code===200){
+    return tickerData;
+  }else{
+    throw {
+      response: { status: 404, statusText: `No data found.` },
+    }
+  }
+}
+
 module.exports = {
-    getExchange
+    getExchange,
+    getExchangeList,
+    getExchangeById,
+    getTickers,
+    searchExchange
   };
