68967a8781b492b19cd8bc999115463db3c0a512
KamalNayak7 <90670251+KamalNayak7@users.noreply.github.com>
2022-05-13 22:52:04 -0400

    Update App.js

--- cryptorush/src/App.js
+++ cryptorush/src/App.js
@@ -14,6 +14,7 @@ import AuthProvider from "./firebase/Auth";
 import PrivateRoute from "./Components/PrivateRoutes";
 import Navigation from "./Components/Navigation";
 import SignUp from "./Components/login/SignUp";
+import Exchange from './Components/Exchange'
 
 function App() {
   return (
@@ -59,6 +60,14 @@ function App() {
                   <PrivateRoute>
                     {" "}
                     <Exchanges />
+                  </PrivateRoute>
+                }
+              />
+                <Route
+                path="/Exchanges/:id"
+                element={
+                  <PrivateRoute>
+                    <Exchange />
                   </PrivateRoute>
                 }
               />
